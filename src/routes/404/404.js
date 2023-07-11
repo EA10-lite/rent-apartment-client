00e182154489bf043c0f98aa785716788f2ab832
Emmanuel <emanuelanyigor@gmail.com>
2023-07-11 14:22:30 -0700

    designed the 404 page

--- src/routes/404/404.js
+++ src/routes/404/404.js
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom";
 import "./404.css";
+import desert from "../../assets/desert.jpg";
 
 const NotFound = () => {
     return (
         <div className="app__404">
-            <h1> 404 </h1>
+            <div className="app__404-container section__container">
+                <div className="app__404-content">
+                    <div className="-overlay" />
+                    <div className="-img">
+                        <img src={desert} alt="desert" />
+                    </div>
+                    <div className="-text d-flex flex-column item-center">
+                        <h1> 4<span>0</span><span className="-light">4</span> </h1>
+                        <h3> Nothing to see here! </h3>
+                        <Link to="/">
+                            <span> Back to Home </span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
